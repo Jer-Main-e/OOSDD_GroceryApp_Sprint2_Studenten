eca8f25c5346fb5d039e7f49aa840bb3c6ee9e84
Jer-Main-e <straverspinda@gmail.com>
2025-09-19 22:23:00 +0200

    Login feature Toegevoegd (UC6)
    * Stap 1 en 4 t/m 8 zijn uitgevoerd van UC6.
    
    * Men kan nu het login scherm zien, de gegevens aanpassen en op de login knop drukken. Wanneer men de verkeerde gegevens invuld komt er een foutmelding te staan.
    
    * Alle use cases succesvol uitgewerkt. Men heeft wel voor testing purposes het email adress en wachtwoord op public ingesteld, maar dit zal in de eerst volgende commit verbeterd worden.
    
    * emailAddress en Password private gemaakt.

--- Grocery.Core.Data/Repositories/ClientRepository.cs
+++ Grocery.Core.Data/Repositories/ClientRepository.cs
@@ -1,5 +1,4 @@
-﻿
-using Grocery.Core.Interfaces.Repositories;
+﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
 
 namespace Grocery.Core.Data.Repositories
@@ -16,15 +15,15 @@ namespace Grocery.Core.Data.Repositories
                 new Client(3, "A.J. Kwak", "user3@mail.com", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=")
             ];
         }
-
+        
         public Client? Get(string email)
         {
-            return clientList[0];
+            return clientList.FirstOrDefault(c => c.EmailAddress == email);
         }
 
         public Client? Get(int id)
         {
-            return clientList[0];
+            return clientList.FirstOrDefault(c => c.Id == id);
         }
 
         public List<Client> GetAll()
--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -1,14 +1,26 @@
-﻿
-namespace Grocery.Core.Models
+﻿namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
         private string _emailAddress;
-        private string _password { get; set; }
+        private string _password;
+
+        public string EmailAddress
+        {
+            get => _emailAddress;
+            private set => _emailAddress = value;
+        }
+
+        public string Password
+        {
+            get => _password;
+            private set => _password = value;
+        }
+
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
-            _emailAddress=emailAddress;
-            _password=password;
+            _emailAddress = emailAddress;
+            _password = password;
         }
     }
 }
