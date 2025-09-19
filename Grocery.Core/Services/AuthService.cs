780b1caf2dfe5b051b67eb10898e667ac35d106c
Jer-Main-e <straverspinda@gmail.com>
2025-09-19 21:36:16 +0200

    Alle use cases succesvol uitgewerkt. Men heeft wel voor testing purposes het email adress en wachtwoord op public ingesteld, maar dit zal in de eerst volgende commit verbeterd worden.

--- Grocery.Core/Services/AuthService.cs
+++ Grocery.Core/Services/AuthService.cs
@@ -13,6 +13,12 @@ namespace Grocery.Core.Services
         }
         public Client? Login(string email, string password)
         {
+            var client = _clientService.Get(email);
+
+            if(PasswordHelper.VerifyPassword(password, client._password)){
+                return client;
+            }
+            
             //Vraag de klantgegevens [Client] op die je zoekt met het opgegeven emailadres
             //Als je een klant gevonden hebt controleer dan of het password matcht --> PasswordHelper.VerifyPassword(password, passwordFromClient)
             //Als alles klopt dan klantgegveens teruggeven, anders null
